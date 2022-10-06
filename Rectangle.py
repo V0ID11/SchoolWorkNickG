6ac46899ff8f12b0516fbc839cb5455655ff8e9c
Nick G <nicholas.gurney@abingdon.org.uk>
2022-10-06 15:30:46 +0100

    removed imports  add blank lines

--- Rectangle.py
+++ Rectangle.py
@@ -1,15 +1,16 @@
-from gzip import READ
-from turtle import width
 
 
 class Rectangle:
     def __init__(self,width,height):
         self.width = width
         self.height = height
+
     def get_area(self):
         return round(self.width * self.height,2)
+
     def get_perimeter(self):
         return round(2*self.width + 2* self.height,2)
+        
     def get_diagonal(self):
         return round((self.width**2 + self.height ** 2)**0.5,2)
 

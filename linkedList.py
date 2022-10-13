bae2554c3453546562335bcba789142beb3de91d
Nick Gurney <nicholas.gurney@abingdon.org.uk>
2022-10-13 20:26:33 +0100

    working Linked List I think

---
+++ linkedList.py
@@ -0,0 +1,40 @@
+
+
+class Node:
+    def __init__(self,data,nextNode) -> None:
+        self.data = data
+        self.nextNode = nextNode
+    
+    def get_Data(self):
+        return self.data
+    
+    def set_Next_Node(self,newNode):
+        self.nextNode = newNode
+
+class Stack:
+    def __init__(self) -> None:
+        self.size = 0
+        self.endNode = Node("Empty",0)
+        self.headNode = self.endNode
+        
+    def Pop(self):
+        if self.headNode == self.endNode:
+            print("List Empty")
+            return
+        x = self.headNode
+        self.headNode = self.headNode.nextNode
+        del(x)
+        self.size -= 1
+
+    def Push(self):
+        x = input("What data would you like the new node to contain")
+        newNode = Node(x,self.headNode)
+        self.headNode = newNode
+        self.size += 1
+
+linkedList = Stack()
+linkedList.Push()
+linkedList.Pop()
+linkedList.Pop()
+print(linkedList.headNode.get_Data())
+

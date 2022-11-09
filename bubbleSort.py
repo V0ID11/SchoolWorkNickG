438c4cd1dd42e34985eee6845a1b4a97554ae622
Nick G <nicholas.gurney@abingdon.org.uk>
2022-11-09 11:54:58 +0000

    few changes added exit case for if all swap false

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,11 +3,15 @@ import random
 
 def bubbleSort(toSort):
     length = len(toSort)
+    swapped = False
     for i in range(0,length-1):
         if toSort[i] > toSort[i+1]:
             temp = toSort[i]
             toSort[i] = toSort[i+1]
             toSort[i+1] = temp
+            swapped = True
+    if swapped == False:
+        return toSort
     if length > 2:
         x = toSort[-1]
         toSort = bubbleSort(toSort[0:length-1])
@@ -15,6 +19,6 @@ def bubbleSort(toSort):
     
     return toSort
 
-needsSorting = [random.randint(1,100) for i in range(50)]
+needsSorting = [random.randint(1,1000) for i in range(1000)]
 print(needsSorting)
 print(bubbleSort(needsSorting))

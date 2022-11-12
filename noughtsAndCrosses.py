f0d1355c5d78991f16dc5e60e1072f270b75d97b
Nick Gurney <nicholas.gurney@abingdon.org.uk>
2022-11-12 15:55:23 +0000

    Major bugs that need fixing

--- noughtsAndCrosses.py
+++ noughtsAndCrosses.py
@@ -1,3 +1,5 @@
+import random
+
 class Board:
     def __init__(self) -> None:
         self.grid = [[" "," "," "] for i in range(3)]
@@ -9,38 +11,101 @@ class Board:
             print("    ---------")
         print(f"{2}   {self.grid[2][0]} | {self.grid[2][1]} | {self.grid[2][2]}")
     
-    def updateBoard(self, x, y,marker):
-        self.grid[y][x] = marker
+    def updateBoard(self, x, y, player):
+        self.grid[y][x] = player
 
     def checkSquare(self,x,y):
         if self.grid[y][x] == "":
-            return True
-        else:      
             return False
+        else:      
+            return True
 
     def checkForWin(self):
         for i in range(3):
-            if self.grid[i][0] == self.grid[i][1] == self.grid[i][2] != " ":
-                return f"{self.grid[i][0]}"
-            elif self.grid[0][i] == self.grid[1][i] == self.grid[2][i] != " ":
-                return f"{self.grid[0][i]}"
-        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] != " " or self.grid[0][2] == self.grid[1][1] == self.grid[2][0] != " "
-            return f"{self.grid[1][1]}"
+            if self.grid[i][0] == self.grid[i][1] == self.grid[i][2] and self.grid[i][0] != " ":
+                return True
+            elif self.grid[0][i] == self.grid[1][i] == self.grid[2][i] and self.grid[0][i] != " ":
+                return True
+        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] != " " or self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[2][2]!= " ":
+            return True
+        else:
+            return False
 
 class Player:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, marker) -> None:
+        self.name = name
+        self.marker = marker
+    def __str__(self) -> str:
+        return self.marker
+    def __repr__(self) -> str:
+        return self.name
 
 class Game:
-    def __init__(self,numPlayers):
+    def __init__(self, numPlayers):
         self.board = Board()
         if numPlayers == 1:
-            Player()
+            p1Name = input("What is the name of Player 1")
+            m1 = input("What letter does Player 1 want as marker?")
+            self.player1 = Player(p1Name,m1)
+            if m1.upper() == "X": 
+                self.computer = Player("Computer", "O")
+            else:
+                self.computer = Player("Computer", "X")
+        
         elif numPlayers == 2:
-            self.x = Player()
-            self.y = Player()
+            p1Name = input("What is the name of Player 1")
+            m1 = input("What letter does Player 1 want as marker?")
+            self.player1 = Player(p1Name,m1)
+            p2Name = input("What is the name of Player 1")
+            m2 = input("What letter does Player 1 want as marker?")
+            self.player2 = Player(p2Name,m2)
+            
+    def gameLoopComputer(self):
+        while True:
+            self.board.displayBoard()
+            move = input(f"Where will {self.player1.name} go in the form x,y")
+            x,y = move.split(",")
+            while self.board.checkSquare(int(x), int(y)) == False:
+                move = input(f"Where will {self.player1.name} go in the form x,y")
+                move.split(",")
+            self.board.updateBoard(int(x), int(y), self.player1)
+            self.board.displayBoard()
+            if self.board.checkForWin() == True:
+                return  f"{self.player1.__repr__()} wins"
+            self.board.displayBoard()            
+            x,y = random.randint(0,3),random.randint(0,3)
+            while self.board.checkSquare(int(x), int(y)) == False:
+                x,y = random.randint(0,3),random.randint(0,3)
+            self.board.updateBoard(x,y,self.computer)
+            if self.board.checkForWin() == True:
+                return  f"Computer Wins"
+    def gameLoop2Player(self):
+        while True:
+            self.board.displayBoard()
+            move = input(f"Where will {self.player1.name} go in the form x,y")
+            x,y = move.split(",")
+            while self.board.checkSquare(int(x), int(y)) == False:
+                move = input(f"Where will {self.player1.name} go in the form x,y")
+                move.split(",")
+            self.board.updateBoard(int(x), int(y), self.player1)
+            if self.board.checkForWin() == True:
+                return  f"{self.player1.__repr__()} Wins"
+            self.board.displayBoard()
+            move = input(f"Where will {self.player2.name} go in the form x,y")
+            x,y = move.split(",")
+            while self.board.checkSquare(int(x), int(y)) == False:
+                move = input(f"Where will {self.player2.name} go in the form x,y")
+                move.split(",")
+            self.board.updateBoard(int(x), int(y), self.player2)
+            if self.board.checkForWin() == True:
+                return  f"{self.player2.__repr__()} Wins"
+            self.board.displayBoard()
 
 
 
-game = Board()
-game.displayBoard()
+numPlayer = int(input("How Many Players? "))
+game = Game(numPlayer)
+if numPlayer == 1:
+    print(game.gameLoop2Player())
+else:
+    print(game.gameLoopComputer())

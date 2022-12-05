077c9d8c2ec77cec058d049a83fd509ce53cb098
Nick G <nicholas.gurney@abingdon.org.uk>
2022-12-05 11:32:17 +0000

    more engines

--- EnginesCSV.py
+++ EnginesCSV.py
@@ -6,6 +6,8 @@ class Engine:
 
     def addToCSV(self, file):
         file.write(f"{self.name},{self.weight},{self.colour}\n")
+    def __repr__(self):
+        return f"{self.name},{self.weight},{self.colour}"
 
 
 engines_list = []
@@ -14,10 +16,23 @@ engines_list.append(Engine('James', 650, 'red'))
 engines_list.append(Engine('Edward', 1000, 'blue'))
 engines_list.append(Engine('Gordon', 2500, 'blue'))
 engines_list.append(Engine('Henry', 2100, 'green'))
+def writeToFile():
+    with open("files/Engine_Data.csv", "w") as file:
+        x = Engine("Name","Weight", "Colour")
+        x.addToCSV(file)
+        for e in engines_list:
+            e.addToCSV(file)
+        print("Complete")
+def readFromFile():
+    newEngineList = []
+    with open("files/Engine_Data.csv", "r") as file:
+        for i in file:
+            x = i.strip()
+            newEngineList.append(Engine(x.split(",")[0],x.split(",")[1],x.split(",")[2]))
+    print(newEngineList[1:])
 
-with open("files/Engine_Data.csv", "w") as file:
-    x = Engine("Name","Weight", "Colour")
-    x.addToCSV(file)
-    for e in engines_list:
-        e.addToCSV(file)
-
+x = input("Do you want to read or write from file r/w: ")
+if x == "r":
+    readFromFile()
+elif x == "w":
+    writeToFile()
